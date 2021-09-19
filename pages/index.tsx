2011f73f67ee816da8e2499e37b9ce01b31510ad
Felipe Lima <fearlimasi@gmail.com>
2021-09-19 11:54:38 -0300

    [wip] feat: alterado step final para incluir componente com o resultado

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,17 @@ import React, { useState } from 'react'
 import { 
   Layout, 
   Breadcrumb, 
-  Collapse, 
   Form, 
   Radio,
   Row,
   Col,
   Card,
   Statistic,
-  Descriptions,
   Button,
   Steps,
   message
 } from 'antd';
-import { CaretRightOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { ArrowUpOutlined } from '@ant-design/icons';
 
 import styles from './styles/index.module.css';
 
@@ -28,7 +26,6 @@ import motivoAdmissao from './api/motivoAdmissao.json';
 import variaveis from './api/variaveis.json';
 
 const { Content } = Layout;
-const { Panel } = Collapse;
 const { Step } = Steps;
 
 const steps = [
@@ -54,8 +51,46 @@ const steps = [
   }
 ]
 
+const layoutForm = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const fieldsForm = {
+  idade: 0,
+  diasInternacoesPrevios: 0,
+  procedencia: 0,
+  tratOncologico: 0,
+  cancer: 0,
+  cancerHematologico: 0,
+  insuficienciaCardiaca: 0,
+  cirrose: 0,
+  sidaHIV: 0,
+  drogasVasoativasPrevias: 0,
+  admissaoUTI: 0,
+  cardiovascular: 0,
+  hepatopatia: 0,
+  digestivo: 0,
+  neurologico: 0,
+  cirurgiaNaAdmissao: 0,
+  tipoDeOperacao: 0,
+  infeccaoNosocomial: 0,
+  infeccaoRespiratoria: 0,
+  glasgow: 0,
+  pressaoArterialSistolica: 0,
+  frequenciaCardiaca: 0,
+  temperatura: 0,
+  oxigenacao: 0,
+  bilirrubina: 0,
+  creatinina: 0,
+  leucocitos: 0,
+  ph: 0,
+  plaquetas: 0
+}
+
 const Home = () => {
   const [current, setCurrent] = useState(0);
+  const [formSaps, setFormSaps] = useState(fieldsForm);
   const [form] = Form.useForm();
 
   const next = () => {
@@ -66,18 +101,27 @@ const Home = () => {
     setCurrent(current - 1);
   }
 
-  const formDadosInternacao = {
-    idade: 0,
-    diasInternacoesPrevios: 0,
-    procedencia: 0
+  const onFinish = (values : any) => {
+    //console.log(values)
   }
 
-  const onFinishInternacao = (values : any) => {
-    formDadosInternacao.idade = values.idade
-    formDadosInternacao.diasInternacoesPrevios = values.diasInternacoesPrevios
-    formDadosInternacao.procedencia = values.procedencia
+  const onFormSaps = (event : any) => {
+    const { idade, diasInternacoesPrevios, procedencia } = event
+    //console.log(idade)
+    setFormSaps({
+      ...formSaps, 
+      ['idade']: idade == 'undefined' ? 0 : idade, 
+      ['diasInternacoesPrevios']: diasInternacoesPrevios, 
+      ['procedencia']: procedencia
+    })
+    console.log(formSaps)
+  }
 
-    console.log(formDadosInternacao)
+  const onChange = (event : any) => {
+    //const {name, value} = event.target
+    //setFormSaps({...formSaps, [name]: value})
+    //const values = form.getFieldsValue()
+    //console.log(values)
   }
 
 
@@ -105,71 +149,92 @@ const Home = () => {
                 margin: 0,
               }}
             >
-              <Steps current={current}>
+              <Steps current={current} size="small">
                 {steps.map(itemStep => (
                   <Step key={itemStep.title} title={itemStep.title} />
                 ))}
               </Steps>
               <Form
                 form={form}
+                onFinish={onFinish}
+                name="formSaps3"
+                {...layoutForm}
               >
                 <div className={styles.stepsContent}>
                   
-                    {steps[current].json.map((item) =>
-                      <Form.Item key={item.key} label={`${item.formLabel}:`} name={item.formName}>  
-                        <Radio.Group buttonStyle="solid" name={item.formName}>
-                          {item.respostas.map((resposta) =>
-                            <Radio.Button 
-                              key={resposta.key}
-                              value={resposta.value}
+                    {current < 4 && (
+                      <>
+                        {steps[current].json.map((item) =>
+                          <Form.Item key={item.key} label={`${item.formLabel}:`} name={item.formName}>  
+                            <Radio.Group 
+                              buttonStyle="solid" 
+                              name={item.formName}
+                              onChange={onChange}
                             >
-                              {resposta.label}
-                            </Radio.Button>  
-                          )}
-                        </Radio.Group>
-                      </Form.Item>
+                              {item.respostas.map((resposta) =>
+                                <Radio.Button 
+                                  key={resposta.key}
+                                  value={resposta.value}
+                                >
+                                  {resposta.label}
+                                </Radio.Button>  
+                              )}
+                            </Radio.Group>
+                          </Form.Item>
+                        )}
+                      </>
+                    )}
+
+                    {current == 4 && (
+                      <>
+                        <Row gutter={16} style={{textAlign: "center"}}>
+                          <Col span={24}>
+                            <Card>
+                              <Statistic
+                                title="Active"
+                                value={11.28}
+                                precision={2}
+                                valueStyle={{ color: '#3f8600' }}
+                                prefix={<ArrowUpOutlined />}
+                                suffix="%"
+                              />
+                            </Card>
+                          </Col>
+                        </Row>
+                      </>
                     )}
                 </div>
+                <Form.Item>
+                  <div className={styles.stepsAction}>
+                      {current < steps.length - 2 && (
+                        <Button type="primary" onClick={() => {
+                          next()
+                          onFormSaps(form.getFieldsValue())
+                        }}>
+                          Avançar
+                        </Button>
+                      )}
+                      {current === steps.length - 2 && (
+                        <Button type="primary" htmlType="submit" onClick={() => {
+                            message.success('SAPS 3 finalizado!')
+                            next()
+                            onFormSaps(form.getFieldsValue())
+                          }}>
+                          Finalizar
+                        </Button>
+                      )}
+                      {current > 0 && (
+                        <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+                          Voltar
+                        </Button>
+                      )}
+                  </div>
+                </Form.Item>
               </Form>
-              <div className={styles.stepsAction}>
-                {current < steps.length - 2 && (
-                  <Button type="primary" onClick={() => next()}>
-                    Avançar
-                  </Button>
-                )}
-                {current === steps.length - 2 && (
-                  <Button type="primary" onClick={() => {
-                      message.success('SAPS 3 finalizado!')
-                      next()
-                    }}>
-                    Finalizar
-                  </Button>
-                )}
-                {current > 0 && (
-                  <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
-                    Voltar
-                  </Button>
-                )}
-              </div>
             </Content>
           </Layout>
 
-          <Layout style={{ padding: '24px 0' }}>
-            <Row gutter={16}>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title="Active"
-                    value={11.28}
-                    precision={2}
-                    valueStyle={{ color: '#3f8600' }}
-                    prefix={<ArrowUpOutlined />}
-                    suffix="%"
-                  />
-                </Card>
-              </Col>
-            </Row>
-          </Layout>
+          
         </Content>
 
         <FooterComponent />
